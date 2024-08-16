eacacd5bb7d0218d039f1eeb94a47314862b7dd5
Mohammed Faheem <xdevfaheem@gmail.com>
2024-08-16 21:44:56 +0530

    update pkg version

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from .spacy_semantic_chunker import SpacySemanticChunker
 from .spacy_syntactic_chunker import SpacySyntacticChunker
 from .syntactic_chunker import SyntacticChunker
 
-package_version = "1.0.0"
+package_version = "0.1"
 
 __all__ = [
     "Chunk",
